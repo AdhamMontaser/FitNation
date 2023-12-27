91526acb0fdf11b2fad6e706ddc8edabd1458beb
3omdawy11 <mohamedzaky970@gmail.com>
2023-12-27 21:24:56 +0200

    my DB

--- DB/fitnation.sql
+++ DB/fitnation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2023 at 10:50 AM
+-- Generation Time: Dec 27, 2023 at 08:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,6 +36,34 @@ CREATE TABLE `admin` (
   `Email` varchar(30) NOT NULL,
   `Password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`ID`, `Username`, `First_Name`, `Second_Name`, `Phone_Number`, `Email`, `Password`) VALUES
+(123216, 'mozaky11', 'Mohamed', 'Emad', '212121212121', 'momomomomom@gmail.com', 'momo123'),
+(123217, 'asad', 'dsada', 'dsad', '232131', 'dsad@mail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorite_exercises`
+--
+
+CREATE TABLE `favorite_exercises` (
+  `username` varchar(30) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `favorite_exercises`
+--
+
+INSERT INTO `favorite_exercises` (`username`, `id`) VALUES
+('Mozaky11', 6),
+('Mozaky11', 1512),
+('Mozaky11', 3294);
 
 -- --------------------------------------------------------
 
@@ -74,6 +102,16 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`ID`, `First_Name`, `Last_Name`, `Username`, `Password`, `Email`, `Phone_Number`) VALUES
+(12, 'Mohamed', 'Emad', 'Mozaky11', '2yBA4vS/6IPUo', 'mohamedzaky970@gmail.com', 1015118963),
+(13, 'Ali', 'Hesham', 'AliHesham', '2yMJyD0vkovog', 'ali@rgola.com', 1015118954),
+(14, 'dsad', 'dsad', 'Mozaky111', '2yG9q7O7s42BI', 'mohamedzaky970@gmail.com', 1015118965),
+(15, 'adham', 'adham', 'adham', '2yOBG.zch./yw', 'adhamdod2003@gmail.com', 1015118954);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -82,6 +120,12 @@ CREATE TABLE `user` (
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `favorite_exercises`
+--
+ALTER TABLE `favorite_exercises`
+  ADD PRIMARY KEY (`username`,`id`);
 
 --
 -- Indexes for table `gym`
@@ -103,7 +147,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123216;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123218;
 
 --
 -- AUTO_INCREMENT for table `gym`
@@ -115,7 +159,7 @@ ALTER TABLE `gym`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
