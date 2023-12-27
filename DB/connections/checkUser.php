e42c6cb763457c31fb0f3dfc14444fd462f1dc8c
3omdawy11 <mohamedzaky970@gmail.com>
2023-12-27 18:36:36 +0200

    yarab ya ali

--- DB/connections/checkUser.php
+++ DB/connections/checkUser.php
@@ -1,49 +1,11 @@
 <?php
-// include 'config.php'; 
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $uname = $_POST['uname'];
-//     $password = $_POST['pass'];
-
-
-
-
-//     if (!empty($uname) && !empty($password)) {
-//         $sql = "SELECT * FROM user WHERE Username='$uname'";
-//         $result = $con->query($sql);
-
-//         if ($result->num_rows > 0) {
-//             $user = $result->fetch_assoc();
-
-//             if (password_verify($password, $user['Password'])) {
-
-//                 echo "Login successful!";
-
-//             } else {
-
-//                 echo "Incorrect password!";
-//             }
-//         } else {
-
-//             echo "Username not found!";
-//         }
-//     } else {
-
-//         echo "Username and password are required!";
-//     }
-// }
-
-
-
-
-//this is more optimized preventable sql injection using prepared stmt and mysqli_real_escape_string
 
 $con = new mysqli('localhost', 'root', '', 'fitnation');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $uname = $_POST['uname'];
     $password = $_POST['pass'];
-    $isFound = "-1";
+    // $isFound = "-1";
 
     if (!empty($uname) && !empty($password)) {
 
@@ -65,24 +27,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION['user'] = $user;
                 header("location: ../../index.php");
             } else {
-                // include "../../login.php";
                 $isFound = "1";
-                header("Location: ../../login.php");
+                session_start();
+                $_SESSION['isFound'] = $isFound;
+                header("location: ../../login.php");
             }
         } else {
-            // include "../../login.php";
-            $isFound = "0";
-            header("Location: ../../login.php");
+            $isFound = "100";
+            session_start();
+            $_SESSION['isFound'] = $isFound;
+            header("location: ../../login.php");
         }
         $stmt->close();
+    } else {
+        $isFound = "-1";
+        session_start();
+        $_SESSION['isFound'] = $isFound;
+        header("location: ../../login.php");
     }
 }
-?>
-<script>
-    const isFound = <?php echo $isFound ?>;
-
-    if (isFound == "1")
-        document.getElementById("checkUser").innerHTML = "Username or Password are incorrect";
-    else if (isFound == "0")
-        document.getElementById("checkUser").innerHTML = "User not found. Try signing up";
-</script>
--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <link rel="stylesheet" href="css/login.css" />
+  <link id="css" rel="stylesheet" href="css/login.css" type="text/css" />
   <script>
     const text = "Welcome Back !";
     const delay = 100;
@@ -19,7 +19,7 @@
         element.style.borderRight = "none";
       }
     }
-
+    // ALIII
     window.onload = function() {
       typeWriter(text, 0);
     };
@@ -27,80 +27,40 @@
   <title>Login</title>
 </head>
 
-<?php
-$uname = $pass = "";
-$firstRowEr = $secondRowEr = "";
-$isValid = "-1";
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-  if (empty($_POST['uname'])) {
-    $firstRowEr = "Field required";
-    $isValid = "0";
-  } else {
-    $uname = $_POST["uname"];
-    if (!preg_match("/^[a-zA-Z0-9-_].{1,30}$/", $uname)) {
-      $firstRowEr = "at most 30 characters";
-      $isValid = "0";
-    } else {
-      $isValid = "2";
-    }
-  }
-
-  if (empty($_POST["pass"])) {
-    $secondRowEr = "Field required";
-    $isValid = "0";
-  } else {
-    $pass = $_POST["pass"];
-    if (!preg_match("/^[a-zA-Z0-9-_]{1,30}$/", $pass)) {
-      $secondRowEr = "Password must be at most 30 characters";
-      $isValid = "0";
-    } else {
-      $isValid = "2";
-    }
-  }
-
-  function cleanData($data)
-  {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-}
-?>
-
 <body class="bg">
   <div class="div1">
     <p id="welcomeback">Welcome Back !</p>
     <br />
 
-    <form action="" method="post" id="loginForm">
-      <input id="username" type="text" name="uname" placeholder="Enter Username..." value="<?php echo $uname; ?>" />
-      <br>
-      <span class="error"><?php echo $firstRowEr; ?></span>
-      <br>
+    <form action="DB/connections/checkUser.php" method="post" id="loginForm">
+      <input id="username" type="text" name="uname" placeholder="Enter Username..." />
 
-      <input id="password" type="password" name="pass" placeholder="Password" value="<?php echo $pass; ?>" />
-
-      <br>
-      <span class="error"><?php echo $secondRowEr; ?></span>
+      <input id="password" type="password" name="pass" placeholder="Password" />
 
       <p>
         <input id="rememberme" type="checkbox" name="rememberme" checked />
         <label id="rememberme">Remember me</label>
       </p>
 
-      <?php if ($isValid == "-1") { ?>
-        <button id="login-button" name="login">Validate</button>
-      <?php } else if ($isValid == "0") { ?>
-        <span class="error" id="thirdRowEr"><?php echo "Something went wrong "; ?></span><br>
-        <button id="login-button" name="login">Validate</button>
-      <?php } else if ($isValid == "2") { ?>
-        <span class="error" id="thirdRowEr"><?php echo "All good "; ?></span><br>
-        <span class="error" id="checkUser"><?php echo ""; ?></span><br>
-        <button id="login-button" name="login">Login</button>
-      <?php } ?>
+      <?php
+
+      session_start();
+      if (!empty($_SESSION['isFound'])) {
+        $isFound = $_SESSION['isFound'];
+        if ($_SESSION['isFound'] == "100") { ?>
+          <span class="error" id="checkUser">User not found. Try signing up</span><br>
+        <?php } else if ($_SESSION['isFound'] == "-1") { ?>
+          <span class="error" id="checkUser">Fields required</span><br>
+        <?php } else if ($_SESSION['isFound'] == "1") { ?>
+          <span class="error" id="checkUser">Username or Password are incorrect</span><br>
+      <?php }
+      }
+      session_destroy();
+
+      ?>
+
+      <br>
+      <button id="login-button" name="login">Login</button>
 
       <hr id="hline" />
       <a id="forgetpass" href="forgetpassword.php">Forget password?</a>
@@ -108,20 +68,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </form>
   </div>
 
-  <script>
-    const isValid = <?php echo $isValid ?>;
-    document.getElementById("login-button").onclick = function() {
-      if (isValid == "0") {
-        const form = document.getElementById('loginForm');
-        form.action = "login.php";
-        document.form.submit();
-      } else if (isValid == "2") {
-        const form = document.getElementById('loginForm');
-        form.action = "DB/connections/checkUser.php";
-        document.form.submit();
-      }
-    }
-  </script>
 </body>
 
 </html>
